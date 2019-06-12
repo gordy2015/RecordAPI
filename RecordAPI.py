c4b9ce7eca942880d9e1981276b4d023b3460c73
kiki <kiki@kiki.tmz>
2019-06-12 11:09:58 +0800

    增加字段

--- RecordAPI.py
+++ RecordAPI.py
@@ -11,8 +11,8 @@ db.init_app(app)
 api = Api(app)
 
 #如果不存在表就新建此表
-# with app.app_context():
-#     db.create_all()
+#with app.app_context():
+#    db.create_all()
 
 # resource_fields = {
 #             'ip': fields.String,
@@ -58,39 +58,47 @@ class Mrecord(Resource):
         i = request.json.get('incsize')
         j = request.json.get('to_f01')
         k = request.json.get('mark')
+        l = request.json.get('to_f01_costtime')
+        m = request.json.get('baktype')
+        print(l)
         # print(type(r),str(r))
         # t = {'ip': a,'bakname':b}
         #获取上一次的文件大小
-        if lastf:
-            w = Bakfile.query.order_by(Bakfile.id.desc()).limit(1)
+        if lastf and c:
+            w = Bakfile.query.filter_by(bakdir=c).order_by(Bakfile.id.desc()).limit(1)
             last_filesize = 0
             for i in w:
                 last_filesize = i.filesize
             return last_filesize
 
-
         #新增一条记录
-        if a and b:
+        if f and g:
             timeArray = time.localtime(int(f))
             f = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
             timeArray = time.localtime(int(g))
             g = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+        if l:
+            record = Bakfile(ip=a, bakname=b, bakdir=c, md5sum=d, filesize=e, starttime=f,
+                             stoptime=g, costtime=h, incsize=i, to_f01=j, mark=k, to_f01_costtime=l, baktype=m)
+        elif d and e:
             record = Bakfile(ip=a, bakname=b, bakdir=c, md5sum=d, filesize=e, starttime=f,
                              stoptime=g, costtime=h, incsize=i, to_f01=j, mark=k)
-            try:
-                db.session.add(record)
-                db.session.commit()
-                code = 200
-                result = "RECORD SUCCESS"
-            except:
-                result = "RECORD FAILE"
-                code = 600
         else:
-            result = "not ip or bakname or starttime or stoptime"
+            result = "not filesize or md5sum or to_f01_costtime"
             code = 601
+            return result,code
+
+        try:
+            db.session.add(record)
+            db.session.commit()
+            code = 200
+            result = "RECORD SUCCESS"
+        except:
+            result = "RECORD FAILE"
+            code = 600
         return result,code
 
 api.add_resource(Mrecord, '/mrecord')
 
 if __name__ == '__main__':
-    app.run(debug=True,host="192.168.74.104")
+    app.run(debug=True,host="192.168.226.203")
--- models.py
+++ models.py
@@ -6,21 +6,22 @@ import datetime
 db = SQLAlchemy()
 
 class Bakfile(db.Model):
-    __tablename__ = 'bakfile'
+    __tablename__ = 'dbbak'
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
     ip = db.Column(db.String(20))
     bakname = db.Column(db.String(50))
     bakdir = db.Column(db.String(150))
     md5sum = db.Column(db.String(50))
     filesize = db.Column(db.BigInteger)
-    starttime = db.Column(db.DateTime)  #保存时间戳
+    starttime = db.Column(db.DateTime) 
     stoptime = db.Column(db.DateTime)
     costtime = db.Column(db.Integer)
     baktype = db.Column(db.Integer,default=1) #1全量 0增量
-    incsize = db.Column(db.BigInteger)
-    has_restore = db.Column(db.Boolean,default=0) #1有定期还原 0无还原
+    incsize = db.Column(db.BigInteger,nullable=True)
+    has_restore = db.Column(db.Integer,default=0) #1有定期还原 0无还原
     to_f01 = db.Column(db.Integer) #1成功 0失败
-    mark = db.Column(db.String(50))
+    to_f01_costtime = db.Column(db.BigInteger,nullable=True)
+    mark = db.Column(db.String(20))
     # def __init__(self,ip,bakname, bakdir,md5sum,filesize,starttime,stoptime,costtime,baktype,incsize,has_restore,mark):
     #     self.ip = ip
     #     self.bakname = bakname
